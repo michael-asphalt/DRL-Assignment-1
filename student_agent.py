8768158564533ac139feccb3b646e48c9dc5717a
Michael <michaelwu111111@gmail.com>
2025-03-22 15:37:36 +0800

    attempt 26 q learning debug

--- student_agent.py
+++ student_agent.py
@@ -102,12 +102,14 @@ def get_action(obs):
             action = 3
         else: 
             action = 0
-    if action == 4:
-        print("step", step, flush=True)
-        print("PICKUP", pickup, flush=True)
-    elif action ==5:
-        print("step", step, flush=True)
-        print("DROP", pickup, flush=True)
+    print("step", step, flush=True)
+    print(action, flush=True)
+    # if action == 4:
+    #     print("step", step, flush=True)
+    #     print("PICKUP", pickup, flush=True)
+    # elif action ==5:
+    #     print("step", step, flush=True)
+    #     print("DROP", pickup, flush=True)
     return action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 
