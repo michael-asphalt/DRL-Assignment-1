db02ef48d8ccd19d28562f6676f7e2be2db00bf8
Michael <michaelwu111111@gmail.com>
2025-03-22 19:13:36 +0800

    attempt 29 q learning all grid size

--- train.py
+++ train.py
@@ -64,8 +64,8 @@ def get_state(obs, pickup, pickup_pos_idx, drop_pos_idx):
 
     return tuple(ret)
 
-def tabular_q_learning(episodes=5000, alpha=0.075, gamma=0.99,
-                         epsilon_start=1.0, epsilon_end=0.1, decay_rate=0.9998,
+def tabular_q_learning(episodes=5000, alpha=0.05, gamma=0.99,
+                         epsilon_start=1.0, epsilon_end=0.1, decay_rate=0.9997,
                          fuel_limit=5000, grid_size=5):
     env = SimpleTaxiEnv(grid_size=grid_size, fuel_limit=fuel_limit)
     env.action_space = ActionSpace(6)
@@ -99,7 +99,6 @@ def tabular_q_learning(episodes=5000, alpha=0.075, gamma=0.99,
             shaped_reward = 0
             # whether pickup or not
             # decide pickup_pos_idx, drop_pos_idx (0, 1, 2, 3)
-            
             if not pickup:
                 flag = (obs[0] == obs[2 + 2 * pickup_pos_idx] and obs[1] == obs[3 + 2 * pickup_pos_idx])
                 if flag and obs[14] != 1:
@@ -112,8 +111,10 @@ def tabular_q_learning(episodes=5000, alpha=0.075, gamma=0.99,
                             shaped_reward += 25
                             has_pickuped = 1
                 else:
-                    if action == 4 or action == 5:
+                    if action == 4:
                         shaped_reward -= 5
+                    if action == 5:
+                        shaped_reward -= 10
             else:
                 flag = (obs[0] == obs[2 + 2 * drop_pos_idx] and obs[1] == obs[3 + 2 * drop_pos_idx])
                 if flag and obs[15] != 1:
@@ -132,7 +133,9 @@ def tabular_q_learning(episodes=5000, alpha=0.075, gamma=0.99,
                             shaped_reward += 50
                             has_dropped = 1
                 else:
-                    if action == 4 or action == 5:
+                    if action == 5:
+                        shaped_reward -= 5 
+                    if action == 4:
                         shaped_reward -= 10
 
 
@@ -168,48 +171,46 @@ def tabular_q_learning(episodes=5000, alpha=0.075, gamma=0.99,
     return q_table, rewards_per_episode
 
 if __name__ == "__main__":
-    episodes = 15000  
-    q_table, rewards = tabular_q_learning(episodes=episodes, grid_size=5)
-    q_table = dict(q_table)
-    file_name = "q_table.pkl"
-    with open(file_name, "wb") as f:
-        pickle.dump(q_table, f)
-    print("Q-table saved to q_table.pkl")
-    # episodes = 10000 
-    # all_q_tables = {}
-    # all_rewards = {}
+    # episodes = 15000  
+    # q_table, rewards = tabular_q_learning(episodes=episodes, grid_size=5)
+    # q_table = dict(q_table)
+    # file_name = "q_table.pkl"
+    # with open(file_name, "wb") as f:
+    #     pickle.dump(q_table, f)
+    # print("Q-table saved to q_table.pkl")
+    episodes = 10000 
+    all_q_tables = {}
+    all_rewards = {}
 
-    # for grid_size in range(5, 11):
-    #     print(f"start training grid_size = {grid_size}")
-    #     q_table, rewards = tabular_q_learning(episodes=episodes, grid_size=grid_size)
-    #     all_q_tables[grid_size] = dict(q_table)
-    #     all_rewards[grid_size] = rewards
+    for grid_size in range(5, 11):
+        print(f"start training grid_size = {grid_size}")
+        q_table, rewards = tabular_q_learning(episodes=episodes, grid_size=grid_size)
+        all_q_tables[grid_size] = dict(q_table)
+        all_rewards[grid_size] = rewards
 
-    #     file_name = f"q_table_{grid_size}.pkl"
-    #     with open(file_name, "wb") as f:
-    #         pickle.dump(dict(q_table), f)
-    #     print(f"Q-table of gridsize = {grid_size} have been saved to {file_name}\n")
+        file_name = f"q_table_{grid_size}.pkl"
+        with open(file_name, "wb") as f:
+            pickle.dump(dict(q_table), f)
+        print(f"Q-table of gridsize = {grid_size} have been saved to {file_name}\n")
     
-    # # merge q_tables
-    # merged_q_table = {}
-    # for grid_size in sorted(all_q_tables.keys()):
-    #     q_table = all_q_tables[grid_size]
-    #     for state, q_values in q_table.items():
-    #         if state not in merged_q_table:
-    #             merged_q_table[state] = (grid_size, q_values)
-    #         else:
-    #             stored_grid_size, _ = merged_q_table[state]
-    #             if grid_size > stored_grid_size:
-    #                 merged_q_table[state] = (grid_size, q_values)
+    # merge q_tables
+    merged_q_table = {}
+    for grid_size in sorted(all_q_tables.keys()):
+        q_table = all_q_tables[grid_size]
+        for state, q_values in q_table.items():
+            if state not in merged_q_table:
+                merged_q_table[state] = (q_values.copy(), 1)
+            else:
+                sum_values, count = merged_q_table[state]
+                merged_q_table[state] = (sum_values + q_values, count + 1)
+
+    final_q_table = {state: q_sum / count for state, (q_sum, count) in merged_q_table.items()}
 
 
-    # final_q_table = {state: q_values for state, (grid_size, q_values) in merged_q_table.items()}
-
-    # # 合併所有資料，並將合併後的 final_q_table 加入其中
-    # merged_file_name = "q_table.pkl"
-    # with open(merged_file_name, "wb") as f:
-    #     pickle.dump(final_q_table, f)
-    # print(f"all grid_size Q-table have been merged, and saved to {merged_file_name}")
+    merged_file_name = "q_table.pkl"
+    with open(merged_file_name, "wb") as f:
+        pickle.dump(final_q_table, f)
+    print(f"all grid_size Q-table have been merged, and saved to {merged_file_name}")
 
     # plt.plot(rewards)
     # plt.xlabel("Episode")
